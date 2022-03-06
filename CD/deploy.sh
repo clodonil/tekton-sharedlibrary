c3ee0fc641c9dd46ddf5ccc2447b90a5b05bf81a
Clodonil Trigo <clodonil@nisled.org>
2022-03-06 19:50:48 -0300

    Adicionando o deploy

--- CD/deploy.sh
+++ CD/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubectl
+#apt-get update
+#apt-get install -y apt-transport-https ca-certificates curl
+#curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+#apt-get update
+#apt-get install -y kubectl
 cp /workspace/sharedlibrary/CD/deploy.yaml .
 sed -i -e "s;__imgae__;$(params.app-image);g" deploy.yaml
 sed -i -e "s;__app__;$(params.app-name);g" deploy.yaml
