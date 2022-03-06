6a3bd40509300e839d265c0c1db07196fa292e80
Clodonil Trigo <clodonil@nisled.org>
2022-03-06 17:17:02 -0300

    Adicionando o sonar

--- CI/python/sonar/sonar.sh
+++ CI/python/sonar/sonar.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "Executando Sonar"
+
+export SONAR_SCANNER_VERSION=4.7.0.2747
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+SONAR_TOKEN='e2fd9312a36a9ec063e6a200e1b9a8a9a2fb5e74'
+
+sonar-scanner \
+  -Dsonar.organization=microservice-api \
+  -Dsonar.projectKey=python-api \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
