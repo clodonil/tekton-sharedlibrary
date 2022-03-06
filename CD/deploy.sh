2f78daaa80d446d5ecec99d37583885656a1f11d
Clodonil Trigo <clodonil@nisled.org>
2022-03-06 19:56:53 -0300

    Adicionando o deploy

--- CD/deploy.sh
+++ CD/deploy.sh
@@ -7,7 +7,7 @@ set -x
 #apt-get update
 #apt-get install -y kubectl
 cp /workspace/sharedlibrary/CD/deploy.yaml .
-sed -i -e "s;__imgae__;$(params.app-image);g" deploy.yaml
-sed -i -e "s;__app__;$(params.app-name);g" deploy.yaml
+sed -i -e "s;__image__;$(APP-IMAGE);g" deploy.yaml
+sed -i -e "s;__app__;$(APP-NAME);g" deploy.yaml
 cat deploy.yaml
 kubectl apply -f deploy.yaml 
