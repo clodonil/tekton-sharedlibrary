e2e1087e8b349c2efe01e86f13b4ee1042287471
Clodonil Trigo <clodonil@nisled.org>
2022-03-06 17:22:47 -0300

    Adicionando o scan do sonar

--- CI/python/sonar/sonar.sh
+++ CI/python/sonar/sonar.sh
@@ -2,6 +2,9 @@
 
 echo "Executando Sonar"
 
+apt-get update 
+apt-get install -y curl
+
 export SONAR_SCANNER_VERSION=4.7.0.2747
 export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
